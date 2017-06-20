b4783175a13607a7f00a282ffd7fa23bb9becd1a
smaran39 <smaran3939@gmail.com>
2017-06-20 02:09:52 -0700

    added functionality for different days

--- public/app.js
+++ public/app.js
@@ -22,7 +22,7 @@ weatherApp.controller('homeController',['$scope','cityService',function ($scope,
 weatherApp.controller('forecastController',['$scope','$resource','$routeParams','cityService',function ($scope,$resource,$routeParams,cityService) {
 
 $scope.city = cityService.city;
-$scope.days = $routeParams.days || 2;
+$scope.days = $routeParams.days || '2'; //gave 2 as string for bg-primary purpose, see forecast.html file 
 
 $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=d170ad88fe3a864fc42ad617343ff9a4", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" }});
 $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days });
