35b932c08c17efb0338c3f23b052f3b088135c93
smaran39 <smaran3939@gmail.com>
2017-06-21 12:36:23 -0700

    improved UX by adding ngSubmit

--- public/app.js
+++ public/app.js
@@ -10,13 +10,18 @@ weatherApp.service('cityService',function () {
 
 //controllers
 
-weatherApp.controller('homeController',['$scope','cityService',function ($scope,cityService) {
+weatherApp.controller('homeController',['$scope','$location','cityService',function ($scope,$location,cityService) {
 
   $scope.city = cityService.city;
 
   $scope.$watch('city',function () {
     cityService.city = $scope.city;
   });
+
+  $scope.submit = function () {
+    $location.path("/forecast");
+  };
+
 }]);
 
 weatherApp.controller('forecastController',['$scope','$resource','$routeParams','cityService',function ($scope,$resource,$routeParams,cityService) {
